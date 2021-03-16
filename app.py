202e91615add8319191dad6685f1dc30fa68823f
Puttipong1234 <Puttipong.lims@gmail.com>
2021-03-16 14:39:14 +0700

    Add

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 import time
 from binance_connect import SIGNALS_BY_SYMBOLS
+import threading
+
+
+
+
 
 try:
     from config import *
@@ -9,22 +14,77 @@ except:
 
 app = Flask(__name__)
 
-def job():
-    print("CHECKING FOR SIGNALS PLEASE WAITS")
 
-    coin_list = ["BTCUSDT","ETHUSDT","BCHUSDT","LTCUSDT","DOGEUSDT","DOTUSDT","ADAUSDT","BNBUSDT"]
-    for coin in coin_list:
-        print(coin)
-        r = SIGNALS_BY_SYMBOLS(coin)
-        if r == "BUY":
-            print("BUY NOW")
-        
-        elif r == "SELL":
-            print("SELL NOW")
-        
-        else:
-            print("NO SIGNALS")
 
+run = True
+
+def worker():
+    
+    while run:
+        print(run)
+        print("=======SCANNOW=======")
+        job()
+        time.sleep(10)
+        print("=====================")
+
+class SIGNALS_MORNITORING(threading.Thread):
+    
+    def __init__(self,coins_list = []):
+        self.need_to_break = False
+        self.coins_list = []
+    
+    def job(self):
+        print("CHECKING FOR SIGNALS PLEASE WAITS")
+
+        coin_list = ["BTCUSDT","ETHUSDT","BCHUSDT","LTCUSDT","DOGEUSDT","DOTUSDT","ADAUSDT","BNBUSDT"]
+        for coin in coin_list:
+            print(coin)
+            r = SIGNALS_BY_SYMBOLS(coin)
+            if r == "BUY":
+                print("BUY NOW")
+            
+            elif r == "SELL":
+                print("SELL NOW")
+            
+            else:
+                print("NO SIGNALS")
+    
+    def run(self):
+        while not self.need_to_break:
+            print("=======SCANNOW=======")
+            self.job()
+            time.sleep(10)
+            print("=====================")
+    
+    def stop(self):
+        print("Stop Signals")
+        self.need_to_break = True
+    
+    
+    
+    
+
+
+
+SM = SIGNALS_MORNITORING()
+SM_t = threading.Thread(target=SM.run,daemon=True)
+
+@app.route("/<START>", methods=['POST'])
+def stop_app(START):
+    if START=="START":
+        try:
+            SM_t.start()
+        except:
+            return "PLEASE STOP BEFORE START"
+    
+    else:
+        try:
+            SM.stop()
+            SM_t.join()
+        except:
+            return "PLEASE START BEFORE STOP"
+    
+    return "ok"
 
 
 @app.route("/", methods=['POST'])
@@ -57,22 +117,30 @@ def pair_signals(pairname):
 
 if __name__ == '__main__':
     #app.run(debug=True)
+    app.run()
     
-    input("PRESS ENTER TO RUN BOT")
-    #sched.add_job(job, 'interval', minutes=1, id='SIGNALS_JOBS')
-    #schedule.every(10).seconds.do(job)
-    while True:
-        print("=======SCANNOW=======")
-        job()
-        time.sleep(60)
-        print("=====================")
-        #schedule.run_pending()
-        #if input("PRESS ENTER TO EXIT"):
-        #    sched.remove_job('SIGNALS_JOBS')
-        #    sched.shutdown()
-        #   break
+    # input("PRESS ENTER TO RUN BOT")
+    # SM = SIGNALS_MORNITORING()
+    # SM_t = threading.Thread(target=SM.run,daemon=True)
+    # SM_t.start()
+    # input("PRESS ENTER TO STOP BOT")
+    # SM.stop()
+    # SM_t.join()
+    # #sched.add_job(job, 'interval', minutes=1, id='SIGNALS_JOBS')
+    # #schedule.every(10).seconds.do(job)
+    # while True:
+    #     print("=======SCANNOW=======")
+    #     job()
+    
+    #     time.sleep(60)
+    #     print("=====================")
+    #     #schedule.run_pending()
+    #     #if input("PRESS ENTER TO EXIT"):
+    #     #    sched.remove_job('SIGNALS_JOBS')
+    #     #    sched.shutdown()
+    #     #   break
 
-    print("THANK FOR YOUR KIND ATTENDTION")
+    # print("THANK FOR YOUR KIND ATTENDTION")
         
         
 
---
+++ test.py
@@ -0,0 +1,5 @@
+import requests
+
+if __name__ == '__main__':
+    r = requests.post(url="http://127.0.0.1:5000/STOP")
+    
